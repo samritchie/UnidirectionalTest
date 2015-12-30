0d23cc35307b5ddc58c711b799323afc16596505
Sam Ritchie <sam@codesplice.com.au>
2015-12-30 08:27:09 +0800

    Initial Commit

---
+++ UnidirectionalTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnidirectionalTest
+//
+//  Created by Sam Ritchie on 30/12/2015.
+//  Copyright © 2015 codesplice pty ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
